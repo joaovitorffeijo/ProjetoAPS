6738ccfa8857eb41771fe34ba3251f67493706c7
Pvazevedo <79871889+Pvazevedo@users.noreply.github.com>
2023-02-03 00:41:22 -0300

    Telas Artigos-Cad, Eventos e Eventos-Cad
    Fiz algumas pequenas atualizações que estão avisadas como comentário na style.css

--- views/artigos.php
+++ views/artigos.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
-    <link rel="stylesheet" href="./css/artigos.css">
     <title>Artigos</title>
 </head>
 <body>
@@ -13,6 +12,8 @@
             <img src="" alt="logo" class="logo">
             <nav class="menu-nav">
                 <ul class="navlist">
+                    <li><a href="./eventos.php">Eventos</a></li>
+                    <li><a href="./artigos.php">Artigos</a></li>
                     <li><a href="#QUEM SOMOS">Quem Somos</a></li>
                     <li><a href="./colaboradores">Colaboradores</a></li>
                     <li><img src="./img/user-128.png" alt="user"><a href="./login.html">Login |</a><a href="./cadastro.html"> Cadastre-se</a></li>
@@ -46,39 +47,39 @@
         </div>
     </div>
 
-    <div class="filtro-artigo">
-        <div class="titulo-filtro-artigo">
+    <div class="filtro">
+        <div class="titulo-filtro">
             <H3>Filtro de pesquisa '2022'</H3>
         </div>
-        <div class="box-lista-artigo">
-            <div class="lista-artigo">
+        <div class="lista-filtro">
+            <div class="lista">
             </div>
-            <div class="lista-artigo">
+            <div class="lista">
                 <h3>Artigo 1</h3>
             </div>
-            <div class="lista-artigo">
+            <div class="lista">
                 <h3>Artigo 2</h3>
             </div>
-            <div class="lista-artigo">
+            <div class="lista">
                 <h3>Artigo 3</h3>
             </div>
-            <div class="lista-artigo">
+            <div class="lista">
                 <h3>Artigo 4</h3>
             </div>
-            <div class="lista-artigo">
+            <div class="lista">
                 <h3>Artigo 5</h3>
             </div>
-            <div class="lista-artigo">
+            <div class="lista">
                 <h3>Artigo 6</h3>
             </div>
-            <div class="lista-artigo">
+            <div class="lista">
                 <h3>Artigo 7</h3>
             </div>
-            <div class="lista-artigo">
+            <div class="lista">
                 <h3>Artigo 8</h3>
             </div>
         </div>
-        <div class="pagina-lista-artigo">
+        <div class="pagina-lista">
             <p class="guia">Voltar</p>
             <p class="numero-pagina">1</p>
             <p class="numero-pagina">2</p>
--- views/eventos-Cad.php
+++ views/eventos-Cad.php
@@ -4,9 +4,98 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Artigos</title>
 </head>
 <body>
+    <header>
+            <img src="" alt="logo" class="logo">
+            <nav class="menu-nav">
+                <ul class="navlist">
+                    <li><a href="./eventos.php">Eventos</a></li>
+                    <li><a href="./artigos.php">Artigos</a></li>
+                    <li><a href="#QUEM SOMOS">Quem Somos</a></li>
+                    <li><a href="./colaboradores">Colaboradores</a></li>
+                    <li><img src="./img/user-128.png" alt="user"><a href="./config.html">Olá, Usuário!</a></li>
+                </ul>
+            </nav>
+    </header>
+
+    <div class="pesquisa">
+        <div class="caixa-de-busca">
+            <img src="./img/lupa.png" alt="lupa">
+            <input type="text" id="busca" placeholder="Pesquise pelo nome">
+        </div>
+        <h1>ENCONTRE O ARTIGO</h1>
+        <div class="anos">
+            <a><p class="ano">2022</p></a>
+            <a><p class="ano">2022</p></a>
+            <a><p class="ano">2022</p></a>
+            <a><p class="ano">2022</p></a>
+            <a><p class="ano">2022</p></a>
+            <a><p class="ano">2022</p></a>
+            <a><p class="ano">2022</p></a>
+            <a><p class="ano">2022</p></a>
+            <a><p class="ano">2022</p></a>
+            <a><p class="ano">2022</p></a>
+            <a><p class="ano">2022</p></a>
+            <a><p class="ano">2022</p></a>
+            <a><p class="ano">2022</p></a>
+            <a><p class="ano">2022</p></a>
+            <a><p class="ano">2022</p></a>
+            <a><p class="ano">2022</p></a>
+        </div>
+    </div>
+
+    <div class="filtro">
+        <div class="titulo-filtro">
+            <H3>Filtro de pesquisa '2022'</H3>
+        </div>
+        <div class="lista-filtro">
+            <div class="lista">
+            </div>
+            <div class="lista">
+                <h3>Evento 1</h3>
+            </div>
+            <div class="lista">
+                <h3>Evento 2</h3>
+            </div>
+            <div class="lista">
+                <h3>Evento 3</h3>
+            </div>
+            <div class="lista">
+                <h3>Evento 4</h3>
+            </div>
+            <div class="lista">
+                <h3>Evento 5</h3>
+            </div>
+            <div class="lista">
+                <h3>Evento 6</h3>
+            </div>
+            <div class="lista">
+                <h3>Evento 7</h3>
+            </div>
+            <div class="lista">
+                <h3>Evento 8</h3>
+            </div>
+        </div>
+        <div class="pagina-lista">
+            <p class="guia">Voltar</p>
+            <p class="numero-pagina">1</p>
+            <p class="numero-pagina">2</p>
+            <p class="numero-pagina">3</p>
+            <p class="numero-pagina">4</p>
+            <p class="numero-pagina">5</p>
+            <p class="guia">Próxima pagina</p>
+        </div>
+
+
+    </div>
+
+    <footer>
+        <p>© UFRRJ 2022. All Rights Reserved.</p>
+        <img src="./img/rural_logo_branco.png" alt="logo-rural">
+    </footer>
     
 </body>
 </html>
