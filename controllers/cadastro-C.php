9a0480bad9258b696c40bd69d13dba9e4abf8518
João Vítor da F. Feijó <joaovitorffeijo@pop-os.localdomain>
2023-03-05 10:45:40 -0300

    cadastro Usuario

--- controllers/cadastro-C.php
+++ controllers/cadastro-C.php
@@ -1,5 +1,35 @@
 <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $cargo = input_data($_POST["cargo"]);
-    }   
+
+include('../model/Usuario.php');
+
+function input_data($data) {  
+    $data = trim($data);  
+    $data = stripslashes($data);  
+    $data = htmlspecialchars($data);  
+    return $data;  
+}
+
+$nome = $sexo = $email = $senha = $tel = $lattes = $formacao = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nome = input_data($_POST["nome"]);
+        $sexo = input_data($_POST["sexo"]);
+        $email = input_data($_POST["email"]);
+        $senha = input_data($_POST["senha"]);
+        $tel = input_data($_POST["tel"]);
+        $lattes = input_data($_POST["lattes"]); 
+        $formacao = input_data($_POST["formacao"]);
+}
+
+if ($nome != "" 
+&& $sexo != "" 
+&& $email != "" 
+&& $senha != ""
+&& $tel != "" 
+&& $lattes != "" 
+&& $formacao != "") {
+    $usuario = new Usuario();
+    $usuario->cadastroUsuario($nome, $sexo, $email, $senha, $tel, $lattes, $formacao);
+    echo "Cadastrado com sucesso!";
+}
 ?>
